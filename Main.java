bdc4ec74441e42a2a9713e8cc3d6974a1a8376b0
Alex Vergizov <alvergizoff@gmail.com>
2021-07-20 10:04:40 +0300

    Main.java added

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int initialBalance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int currentBalance = initialBalance + transfer;
+        System.out.println(currentBalance);
+    }
+}
